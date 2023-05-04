0423fa9609d36bb8476ae49e13825093fc54ec25
Антон Марченко <marchenko_aa@apteka-aprel.ru>
2023-05-04 16:29:04 +0300

    Реализован функционал checkbox

--- example/src/components/Checkboxes.tsx
+++ example/src/components/Checkboxes.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { CheckBox } from 'rn-ui-kit';
 
 export const CheckBoxes = () => {
+  const [isCheced, setIsChecked] = React.useState(false);
   return (
     <>
+      <CheckBox
+        disabled={false}
+        checked={isCheced}
+        onPress={() => setIsChecked(!isCheced)}
+      />
+      <CheckBox checked={false} />
       <CheckBox disabled={true} checked={true} />
       <CheckBox disabled={true} checked={false} />
-      <CheckBox disabled={false} checked={true} />
-      <CheckBox disabled={false} checked={false} />
     </>
   );
 };
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -3,19 +3,58 @@ import {
   StyleSheet,
   StyleProp,
   ViewStyle,
-  Pressable,
-  PressableProps,
+  TouchableOpacityProps,
+  Text,
+  TouchableOpacity,
 } from 'react-native';
+import { useTheme } from '../theme/ThemeProvider';
 
-export type CheckboxProps = PressableProps & { checked: boolean };
+export type CheckboxProps = TouchableOpacityProps & { checked: boolean };
 
 export const CheckBox: FC<CheckboxProps> = (props) => {
+  const theme = useTheme();
+
+  const stylesCheckBox = StyleSheet.create({
+    initial: {
+      height: 32,
+      width: 32,
+      borderWidth: 1.5,
+      borderColor: theme.background.accent,
+      borderRadius: 5,
+      margin: 5,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    uncheckedUndisabled: {
+      borderColor: theme.background.accent,
+      backgroundColor: 'transparent',
+    },
+    checkedUndisabled: {
+      borderColor: theme.background.accent,
+      backgroundColor: theme.background.accent,
+    },
+    uncheckedDisabled: {
+      borderColor: theme.background.neutralDisable,
+      backgroundColor: 'transparent',
+    },
+    checkedDisabled: {
+      borderColor: theme.background.neutralDisable,
+      backgroundColor: theme.background.neutralDisable,
+    },
+  });
+
   const currentStyle = () => {
-    if (props.checked && props.disabled) {
-      return stylesCheckBox.diabled;
+    if (!props.checked && !props.disabled) {
+      return stylesCheckBox.uncheckedUndisabled;
+    }
+    if (props.checked && !props.disabled) {
+      return stylesCheckBox.checkedUndisabled;
     }
     if (!props.checked && props.disabled) {
-      return stylesCheckBox.checked;
+      return stylesCheckBox.uncheckedDisabled;
+    }
+    if (props.checked && props.disabled) {
+      return stylesCheckBox.checkedDisabled;
     }
   };
 
@@ -25,22 +64,8 @@ export const CheckBox: FC<CheckboxProps> = (props) => {
   ]);
 
   return (
-    <Pressable style={style} {...props}>
-      {props.children}
-    </Pressable>
+    <TouchableOpacity style={style} {...props} activeOpacity={0.7}>
+      {props.checked && <Text>+</Text>}
+    </TouchableOpacity>
   );
 };
-
-const stylesCheckBox = StyleSheet.create({
-  initial: {
-    height: 40,
-    width: 40,
-    borderBotom: 'green',
-  },
-  diabled: {
-    backgroundColor: 'red',
-  },
-  checked: {
-    backgroundColor: 'green',
-  },
-});
