3ff2df9c83465957aa4052eec294656ea16918fe
Nikita Kovantsov <kovancov@gmail.com>
2023-09-09 17:33:43 +0300

    MyToast

--- storybook/.storybook/stories/MyToast.stories.tsx
+++ storybook/.storybook/stories/MyToast.stories.tsx
@@ -35,7 +35,7 @@ const MyToastMeta = {
 
 export default MyToastMeta;
 
-const basicProps: ToastOptions = {
+const errorProps: ToastOptions = {
   type: 'error',
   text: 'Какое-то описание, раскрывающее суть сообщения. Может быть в несколько строчек',
   title: 'Заголовок желательно в одну строчку',
@@ -48,6 +48,57 @@ const basicProps: ToastOptions = {
     onPress: () => {},
   },
 };
-export const Basic = {
-  args: basicProps,
+export const Error = {
+  args: errorProps,
+};
+
+const successProps: ToastOptions = {
+  type: 'success',
+  text: 'Какое-то описание, раскрывающее суть сообщения. Может быть в несколько строчек',
+  title: 'Заголовок желательно в одну строчку',
+  firstAction: {
+    text: 'Отменить',
+    onPress: () => {},
+  },
+  secondAction: {
+    text: 'Подтвердить',
+    onPress: () => {},
+  },
+};
+export const Success = {
+  args: successProps,
+};
+
+const warningProps: ToastOptions = {
+  type: 'warning',
+  text: 'Какое-то описание, раскрывающее суть сообщения. Может быть в несколько строчек',
+  title: 'Заголовок желательно в одну строчку',
+  firstAction: {
+    text: 'Отменить',
+    onPress: () => {},
+  },
+  secondAction: {
+    text: 'Подтвердить',
+    onPress: () => {},
+  },
+};
+export const Warning = {
+  args: warningProps,
+};
+
+const infoProps: ToastOptions = {
+  type: 'info',
+  text: 'Какое-то описание, раскрывающее суть сообщения. Может быть в несколько строчек',
+  title: 'Заголовок желательно в одну строчку',
+  firstAction: {
+    text: 'Отменить',
+    onPress: () => {},
+  },
+  secondAction: {
+    text: 'Подтвердить',
+    onPress: () => {},
+  },
+};
+export const Info = {
+  args: infoProps,
 };
